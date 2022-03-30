305db826ce8ec10b8f1c3aeeac537fe722f0bf39
Jakob <53713395+Jakob-Eichberger@users.noreply.github.com>
2022-03-31 00:42:29 +0200

    Create Vehicle.cs

---
+++ Model/Vehicle.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public VehicleClassification VehicleClassification { get; set; }
+
+        public bool AddedManually { get; set; } = false;
+
+        public List<Stop> Stops { get; set; } = new();
+    }
+
+}
