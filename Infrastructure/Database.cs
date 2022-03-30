dba1be570d1fa6f2a9abe3ae56d28e53b25e4d14
Jakob <53713395+Jakob-Eichberger@users.noreply.github.com>
2022-03-31 00:42:35 +0200

    Update Database.cs

--- Infrastructure/Database.cs
+++ Infrastructure/Database.cs
@@ -1,16 +1,40 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Model;
 
 namespace Infrastructure
 {
     // All the code in this file is included in all platforms.
-    public class Database : DbContext
+    public partial class Database : DbContext
     {
         public Database(DbContextOptions options) : base(options)
         {
         }
 
-        protected Database()
+        public Database()
         {
         }
+
+        public event EventHandler CollectionChanged;
+
+        public virtual DbSet<Stop> Stops => Set<Stop>();
+
+        public virtual DbSet<Vehicle> Vehicles => Set<Vehicle>();
+
+        public void InvokeCollectionChanged() => CollectionChanged?.Invoke(this, new EventArgs());
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseSqlite($"Data Source=\"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}{Path.DirectorySeparatorChar}Loco.sqlite\"");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Vehicle>().OwnsOne(e => e.VehicleClassification);
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
 }
