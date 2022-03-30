400c787c8204b7264b7bc85a6cb17c6f6df169bc
Jakob <53713395+Jakob-Eichberger@users.noreply.github.com>
2022-03-31 00:42:32 +0200

    Create Stop.cs

---
+++ Model/Stop.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+
+namespace Model
+{
+    public class Stop
+    {
+        public int Id { get; set; }
+
+        public string TrainNumber { get; set; } = "";
+
+        public Vehicle Vehicle { get; set; }
+
+        public int VehicleId { get; set; }
+
+        public Station Station { get; set; }
+
+        public int StationId { get; set; }
+
+        public DateTime? Arrival { get; set; } = DateTime.MinValue;
+
+        public DateTime Departure { get; set; } = DateTime.MinValue;
+    }
+
+    public class Station
+    {
+        public int Id { get; set; }
+
+        public string StationName { get; set; }
+
+        public string StationNameAbbr { get; set; }
+
+        public decimal Lat { get; set; }
+
+        public decimal Lng { get; set; }
+
+        public List<Stop> Stops { get; } = new();
+    }
+
+}
