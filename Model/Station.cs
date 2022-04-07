dab23192b1cf9d46c27c2951928b663626fb1b40
Jakob <53713395+Jakob-Eichberger@users.noreply.github.com>
2022-04-07 23:22:29 +0200

    Create Station.cs

---
+++ Model/Station.cs
@@ -0,0 +1,18 @@
+﻿namespace Model
+{
+    public class Station
+    {
+        //public int Id { get; set; }
+
+        public string StationName { get; set; }
+
+        public string StationNameAbbr { get; set; }
+
+        public decimal Lat { get; set; }
+
+        public decimal Lng { get; set; }
+
+        //public List<Stop> Stops { get; } = new();
+    }
+
+}
