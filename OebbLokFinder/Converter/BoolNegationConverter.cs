d9dff331ffc6d954f361c26b89a5d3f99db277cd
Jakob <53713395+Jakob-Eichberger@users.noreply.github.com>
2022-04-07 23:22:22 +0200

    Create BoolNegationConverter.cs

---
+++ OebbLokFinder/Converter/BoolNegationConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OebbLokFinder.Converter
+{
+    public class BoolNegationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value is bool negation ? !negation : null;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => value is bool negation ? !negation : null;
+    }
+}
