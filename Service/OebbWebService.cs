1de6d5d8dff1a3cf671eac19a6e765feadd22e22
Jakob <53713395+Jakob-Eichberger@users.noreply.github.com>
2022-04-07 23:21:55 +0200

    Update OebbWebService.cs

--- Service/OebbWebService.cs
+++ Service/OebbWebService.cs
@@ -1,4 +1,5 @@
-﻿using Infrastructure;
+﻿using HtmlAgilityPack;
+using Infrastructure;
 using Model;
 using Newtonsoft.Json;
 using System;
@@ -7,34 +8,128 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Service
 {
     public class OebbWebService : BaseService
     {
-        public OebbWebService(Database db) : base(db)
+        public OebbWebService(Database db, StationService stationService, VehicleService vehicleService) : base(db)
         {
-
+            StationService = stationService;
+            VehicleService = vehicleService;
         }
+
+        public StationService StationService { get; }
+
+        public VehicleService VehicleService { get; }
 
         private string ApiURL => @"https://konzern-apps.web.oebb.at/lok/index/";
 
-        public async Task<List<Stop>> GetStops(VehicleClassification vehicleClassification)
+        /// <summary>
+        /// Updates the <see cref="Stop"/>s for a given <paramref name="classNumber"/> and <paramref name="serialNumber"/> combination in the database.
+        /// </summary>
+        /// <param name="classNumber"></param>
+        /// <param name="serialNumber"></param>
+        /// <param name="vehicleName"></param>
+        /// <param name="addedManually"></param>
+        /// <returns></returns>
+        public async Task UpdateStopsAsync(int classNumber, int serialNumber, string vehicleName = null, bool addedManually = true)
         {
-            var uri = new Uri(@$"{ApiURL}{vehicleClassification}");
-            HttpClient client = new HttpClient();
-            var response = await client.GetAsync(uri);
-            var repsonse = await new StreamReader(await response.Content.ReadAsStreamAsync()).ReadToEndAsync();
-            IEnumerable<dynamic> List = JsonConvert.DeserializeObject(repsonse) as IEnumerable<dynamic>;
+            Vehicle vehicle = await VehicleService.GetOrCreatVehicleAsync(classNumber, serialNumber, vehicleName, addedManually);
 
-            List.Select(e =>
+            await VehicleService.RemoveStationsFromVehilce(vehicle);
+
+            vehicle.Stops.AddRange((await Task.WhenAll((await GetVehicleJsonMapListAsync(classNumber, serialNumber)).Select(async e => new Stop()
             {
-                IDictionary<string, string> i = e as IDictionary<string, string>;
-                return "";
-            });
+                Vehicle = vehicle,
+                Station = new Station
+                {
+                    StationName = e.Name.Trim(),
+                    StationNameAbbr = e.Abbr.Trim(),
+                    Lat = decimal.Parse(e.Lat),
+                    Lng = decimal.Parse(e.Lng),
+                },
+                Arrival = e.Arrival,
+                Departure = e.Departure
+            }))));
 
-            return new();
+            await VehicleService.UpdateVehilce(vehicle);
         }
+
+        public async Task UpdateStopsForAllVehicles(Action<double>? vehicleLoaded = null)
+        {
+            vehicleLoaded?.Invoke(0);
+            var rnd = new Random();
+            double count = 0;
+            double max = Db.Vehicles.Count();
+            foreach (var vehicle in Db.Vehicles.ToList())
+            {
+                await UpdateStopsAsync(vehicle.ClassNumber, vehicle.SerialNumber, vehicle.Name, vehicle.AddedManually);
+                await Task.Delay(new TimeSpan(0, 0, rnd.Next(4, 10)));
+                vehicleLoaded?.Invoke(count++ / max);
+            }
+        }
+
+        private async Task<IEnumerable<VehicleJsonMap>> GetVehicleJsonMapListAsync(int classNumber, int serialNumber)
+        {
+            var uri = new Uri(@$"{ApiURL}{classNumber:D4}.{serialNumber:D4}");
+            var response = await new HttpClient().GetStringAsync(uri);
+            IEnumerable<VehicleJsonMap> List = JsonConvert.DeserializeObject<List<VehicleJsonMap>>(response);
+            return List;
+        }
+
+        public async Task UpdateVehilcesFromLokfinderOebbWebsiteListe()
+        {
+            VehicleService.RemoveAllAutomaticallyAddedVehicles();
+
+            HtmlDocument htmlDoc = new();
+            htmlDoc.LoadHtml(await new HttpClient().GetStringAsync("https://lokfinder.oebb.at/"));
+
+            List<Vehicle> vehicles = new();
+            foreach (var vehicle in htmlDoc.DocumentNode.SelectNodes("//oebb-lokfinder-lok"))
+            {
+                var match = new Regex(@"([0-9]{1,4})\.([0-9]{1,4})").Match(vehicle.GetAttributeValue("unit-number", ""));
+
+                vehicles.Add(new Vehicle
+                {
+                    AddedManually = false,
+                    ClassNumber = int.Parse(match.Groups[1].Value),
+                    SerialNumber = int.Parse(match.Groups[2].Value),
+                    Name = vehicle.GetAttributeValue("label", "")
+                });
+            }
+
+            VehicleService.AddVehicleRange(vehicles);
+        }
+
+        private class VehicleJsonMap
+        {
+            [JsonProperty("abbr")]
+            public string Abbr { get; set; }
+
+            [JsonProperty("arrival")]
+            public DateTime? Arrival { get; set; }
+
+            [JsonProperty("departure")]
+            public DateTime Departure { get; set; }
+
+            [JsonProperty("lat")]
+            public string Lat { get; set; }
+
+            [JsonProperty("lng")]
+            public string Lng { get; set; }
+
+            [JsonProperty("name")]
+            public string Name { get; set; }
+
+            [JsonProperty("train_number")]
+            public string TrainNumber { get; set; }
+
+            [JsonProperty("unit_number")]
+            public string UnitNumber { get; set; }
+        }
+
     }
 }
