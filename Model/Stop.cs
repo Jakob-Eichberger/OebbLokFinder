1c1673112cc3c91e094faf51a3d87fa007bc8362
Jakob <53713395+Jakob-Eichberger@users.noreply.github.com>
2022-04-07 23:22:27 +0200

    Update Stop.cs

--- Model/Stop.cs
+++ Model/Stop.cs
@@ -6,34 +6,17 @@ namespace Model
     {
         public int Id { get; set; }
 
-        public string TrainNumber { get; set; } = "";
-
         public Vehicle Vehicle { get; set; }
 
         public int VehicleId { get; set; }
 
         public Station Station { get; set; }
 
-        public int StationId { get; set; }
-
         public DateTime? Arrival { get; set; } = DateTime.MinValue;
 
         public DateTime Departure { get; set; } = DateTime.MinValue;
-    }
 
-    public class Station
-    {
-        public int Id { get; set; }
-
-        public string StationName { get; set; }
-
-        public string StationNameAbbr { get; set; }
-
-        public decimal Lat { get; set; }
-
-        public decimal Lng { get; set; }
-
-        public List<Stop> Stops { get; } = new();
+        public DateTime Fetched { get; set; } = DateTime.Now;
     }
 
 }
