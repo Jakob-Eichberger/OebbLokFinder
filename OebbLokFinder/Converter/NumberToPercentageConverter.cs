58f3c07cb290dd35d3d5af534295716b75478c77
Jakob <53713395+Jakob-Eichberger@users.noreply.github.com>
2022-04-07 23:22:18 +0200

    Create NumberToPercentageConverter.cs

---
+++ OebbLokFinder/Converter/NumberToPercentageConverter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OebbLokFinder.Converter
+{
+    /// <summary>
+    /// Converts a number to a Percentage Value.
+    /// </summary>
+    /// <remarks>
+    /// If the number is decimal, float or double it will be multiplied by 100.</remarks>
+    internal class NumberToPercentageConverter : IValueConverter
+    {
+
+        /// <summary>
+        /// Converts a number to a Percentage Value.
+        /// </summary>
+        /// <remarks>
+        /// If the number is decimal, float or double it will be multiplied by 100.</remarks>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (decimal.TryParse(value?.ToString(), out var valueDE))
+            {
+                return $"{valueDE * 100:0.##}%";
+            }
+            else if (float.TryParse(value?.ToString(), out var valueF))
+            {
+                return $"{valueF * 100:0.##}%";
+            }
+            else if (double.TryParse(value?.ToString(), out var valueD))
+            {
+                return $"{valueD * 100:0.##}%";
+            }
+            else if (int.TryParse(value?.ToString(), out var valueI))
+            {
+                return $"{valueI}%";
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Not implemented!!!
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+            throw new NotImplementedException();
+    }
+}
