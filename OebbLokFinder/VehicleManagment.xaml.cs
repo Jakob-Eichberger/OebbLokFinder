610bf8969a4efa9be9b2c59068cd2c0a357e4fcb
Jakob <53713395+Jakob-Eichberger@users.noreply.github.com>
2022-04-07 23:22:00 +0200

    Update VehicleManagment.xaml.cs

--- OebbLokFinder/VehicleManagment.xaml.cs
+++ OebbLokFinder/VehicleManagment.xaml.cs
@@ -1,9 +1,73 @@
+using Infrastructure;
+using Service;
+using System.ComponentModel;
+
 namespace OebbLokFinder;
 
-public partial class VehicleManagment : ContentPage
+public partial class VehicleManagment : ContentPage, INotifyPropertyChanged
 {
-	public VehicleManagment()
-	{
-		InitializeComponent();
-	}
+    private bool loadingData = false;
+    private double progress = 0;
+    private TimeSpan timeRemaing = TimeSpan.Zero;
+
+    public VehicleManagment(IServiceProvider serviceProvider)
+    {
+        Db = serviceProvider.GetRequiredService<Database>();
+        WebService = serviceProvider.GetRequiredService<OebbWebService>();
+        BindingContext = this;
+        InitializeComponent();
+    }
+
+    public Database Db { get; set; }
+
+    public DateTime? LastUpdated => Db.Stops.Any() ? Db.Stops.Min(e => e.Fetched) : DateTime.Now;
+
+    public bool LoadingData
+    {
+        get => loadingData; set
+        {
+            loadingData = value;
+            OnPropertyChanged(nameof(LoadingData));
+        }
+    }
+
+    public double Progress
+    {
+        get => progress; set
+        {
+            progress = value;
+            OnPropertyChanged(nameof(Progress));
+        }
+    }
+
+    public TimeSpan TimeRemaing
+    {
+        get => timeRemaing; set
+        {
+            timeRemaing = value;
+            OnPropertyChanged(nameof(TimeRemaing));
+        }
+    }
+
+    public OebbWebService WebService { get; set; }
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+
+        var start = DateTime.Now;
+        TimeRemaing = Timeout.InfiniteTimeSpan;
+        LoadingData = true;
+
+        await WebService.UpdateVehilcesFromLokfinderOebbWebsiteListe();
+        await WebService.UpdateStopsForAllVehicles((p) => Dispatcher.Dispatch(() =>
+        {
+            Progress = p;
+            TimeRemaing = Progress != 0 ? new TimeSpan(0, 0, (int)((DateTime.Now - start).TotalSeconds / Progress)) : Timeout.InfiniteTimeSpan;
+        }));
+
+        LoadingData = false;
+        OnPropertyChanged(nameof(LastUpdated));
+    }
+
 }
+
