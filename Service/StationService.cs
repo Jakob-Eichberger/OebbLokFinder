9010c08f6e16ccb2adf0977c0cf61a0c5d24d30e
Jakob <53713395+Jakob-Eichberger@users.noreply.github.com>
2022-04-07 23:21:57 +0200

    Create StationService.cs

---
+++ Service/StationService.cs
@@ -0,0 +1,18 @@
+﻿using Infrastructure;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class StationService : BaseService
+    {
+        public StationService(Database db) : base(db)
+        {
+        }
+
+    }
+}
