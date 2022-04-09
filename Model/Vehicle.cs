18f5ef9a3ecf6fc50db7c2c2551ad6896d505eb5
Jakob <53713395+Jakob-Eichberger@users.noreply.github.com>
2022-04-09 05:40:15 +0200

    Update Vehicle.cs

--- Model/Vehicle.cs
+++ Model/Vehicle.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Model
 {
-    public class Vehicle
+    public class Vehicle 
     {
         public int Id { get; set; }
 
@@ -23,6 +23,8 @@ namespace Model
         public bool AddedManually { get; set; } = false;
 
         public List<Stop> Stops { get; set; } = new();
+
+        public int Value => int.Parse($"{ClassNumber:D4}{SerialNumber:D4}");
 
         public override bool Equals(object obj) => obj is Vehicle vehicle && ClassNumber == vehicle.ClassNumber && SerialNumber == vehicle.SerialNumber;
 
