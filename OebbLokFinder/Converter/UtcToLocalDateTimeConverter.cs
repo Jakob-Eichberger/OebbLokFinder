54a9d21f43ee8a841e45e95333fe9cddc1cf9607
Jakob <53713395+Jakob-Eichberger@users.noreply.github.com>
2022-04-09 22:18:19 +0200

    Create UtcToLocalDateTimeConverter.cs

---
+++ OebbLokFinder/Converter/UtcToLocalDateTimeConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OebbLokFinder.Converter
+{
+    public class UtcToLocalDateTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null)
+                return null;
+            else if (value is DateTime dt)
+                return $"{TimeZoneInfo.ConvertTimeFromUtc(dt, TimeZoneInfo.Local):dd/MM/yy hh:mm}";
+            else
+                throw new NotSupportedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
