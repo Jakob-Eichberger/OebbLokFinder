b2572154655beef7829580491b27d56507d5ce75
Jakob <53713395+Jakob-Eichberger@users.noreply.github.com>
2022-04-10 02:59:50 +0200

    Update VehicleView.xaml.cs

--- OebbLokFinder/VehicleView.xaml.cs
+++ OebbLokFinder/VehicleView.xaml.cs
@@ -8,14 +8,12 @@ public partial class VehicleView : ContentView
 {
     public VehicleView(IServiceProvider serviceProvider, Vehicle vehicle, Action<VehicleView> valueUpdated)
     {
+        Db = serviceProvider.GetService<Database>();
         Vehicle = vehicle;
         OnDelete = valueUpdated;
         Margin = new Thickness(5);
+        BindingContext = this;
         InitializeComponent();
-        Db = serviceProvider.GetService<Database>();
-        EName.Text = vehicle.Name;
-        ESerialNumber.Text = $"{vehicle.SerialNumber}";
-        EClassNumber.Text = $"{vehicle.ClassNumber}";
     }
 
     public Database Db { get; set; }
@@ -23,14 +21,6 @@ public partial class VehicleView : ContentView
     public Vehicle Vehicle { get; set; }
 
     public Action<VehicleView> OnDelete { get; }
-
-    private void EName_TextChanged(object sender, TextChangedEventArgs e)
-    {
-        Vehicle = Db.Vehicles.FirstOrDefault(e => e.Id == Vehicle.Id);
-        Vehicle.Name = EName.Text;
-        Db.Vehicles.Update(Vehicle);
-        Db.SaveChanges();
-    }
 
     private void BtnDelete_Clicked(object sender, EventArgs e)
     {
@@ -40,19 +30,9 @@ public partial class VehicleView : ContentView
         OnDelete(this);
     }
 
-    private void ESerialNumber_TextChanged(object sender, TextChangedEventArgs e)
+    private void Entry_TextChanged(object sender, TextChangedEventArgs e)
     {
-        Vehicle = Db.Vehicles.FirstOrDefault(e => e.Id == Vehicle.Id);
-        Vehicle.SerialNumber = int.Parse(ESerialNumber.Text);
-        Db.Vehicles.Update(Vehicle);
-        Db.SaveChanges();
-    }
-
-    private void EClassNumber_TextChanged(object sender, TextChangedEventArgs e)
-    {
-        Vehicle = Db.Vehicles.FirstOrDefault(e => e.Id == Vehicle.Id);
-        Vehicle.ClassNumber = int.Parse(EClassNumber.Text);
-        Db.Vehicles.Update(Vehicle);
+        Db.Update(Vehicle);
         Db.SaveChanges();
     }
 }
