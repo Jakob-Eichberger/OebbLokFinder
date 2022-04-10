ef513d64887281c2ad73a6381378546d543ca93e
Jakob <53713395+Jakob-Eichberger@users.noreply.github.com>
2022-04-10 03:43:34 +0200

    Update Database.cs

--- Infrastructure/Database.cs
+++ Infrastructure/Database.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Model;
+using Newtonsoft.Json;
 
 namespace Infrastructure
 {
@@ -21,12 +22,14 @@ namespace Infrastructure
 
         public virtual DbSet<Vehicle> Vehicles => Set<Vehicle>();
 
+        public virtual DbSet<Setting> Settings => Set<Setting>();
+
         public void InvokeCollectionChanged() => CollectionChanged?.Invoke(this, new EventArgs());
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             var path = $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}{Path.DirectorySeparatorChar}LokFinder{Path.DirectorySeparatorChar}";
-            if(!Directory.Exists(path))
+            if (!Directory.Exists(path))
                 Directory.CreateDirectory(path);
 
             if (!optionsBuilder.IsConfigured)
@@ -36,9 +39,23 @@ namespace Infrastructure
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Stop>().OwnsOne(e => e.Station);
+            modelBuilder.Entity<Setting>().Property(b => b.Properties).HasConversion(v => JsonConvert.SerializeObject(v), v => JsonConvert.DeserializeObject<Dictionary<string, string>>(v));
             OnModelCreatingPartial(modelBuilder);
         }
 
         partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+        public void Init()
+        {
+            if (Database.EnsureCreated())
+            {
+                Vehicles.Add(new Model.Vehicle() { ClassNumber = 1116, SerialNumber = 36, Name = "CAT", AddedManually = true });
+                if (!Settings.Any())
+                {
+                    Settings.Add(new Model.Setting());
+                }
+                SaveChanges();
+            }
+        }
     }
 }
