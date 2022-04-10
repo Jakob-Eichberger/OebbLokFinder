c35e5f0de91ad0e06bcc8eb6ec4d3db6b1499c18
Jakob <53713395+Jakob-Eichberger@users.noreply.github.com>
2022-04-10 03:07:07 +0200

    Update LokFinder.xaml.cs

--- OebbLokFinder/LokFinder.xaml.cs
+++ OebbLokFinder/LokFinder.xaml.cs
@@ -21,7 +21,7 @@ public partial class LokFinder : ContentPage, INotifyPropertyChanged
 
     public string Station { get; set; }
 
-    public List<string> Stations => Db.Stops.Select(e => e.Station.StationName).Distinct().OrderBy(e => e).ToList();
+    public List<string> Stations => Db.Stops.Where(e => (e.Arrival ?? e.Departure) >= DateTime.Now).Select(e => e.Station.StationName).Distinct().OrderBy(e => e).ToList();
 
     public OebbWebService WebService { get; set; }
 
@@ -31,32 +31,7 @@ public partial class LokFinder : ContentPage, INotifyPropertyChanged
         var stops = Db.Stops.Include(e => e.Vehicle).Where(e => e.Station.StationName == Station).OrderBy(e => e.Arrival ?? e.Departure);
         foreach (var stop in stops)
         {
-            //VSLStops.Add(new Stop(stop));
             VSLStops.Add(new StopView(stop));
         }
-    }
-}
-
-public class Stop : Frame
-{
-    public Stop(Model.Stop stop)
-    {
-        Padding = new Thickness(8);
-        Margin = new Thickness(5);
-
-        var c = new StackLayout
-        {
-            new Label{Text = $"{stop.Vehicle.ClassNumber}.{stop.Vehicle.SerialNumber}"},
-        };
-        if (!string.IsNullOrWhiteSpace(stop.Vehicle.Name))
-        {
-            c.Add(new Label() { Text = $"{stop.Vehicle.Name}" });
-        }
-        if (stop.Arrival is DateTime a)
-        {
-            c.Add(new Label() { Text = $"Arrival:\t{TimeZoneInfo.ConvertTimeFromUtc(a, TimeZoneInfo.Local):dd/MM/yy hh:mm}" });
-        }
-        c.Add(new Label() { Text = $"Departure: {TimeZoneInfo.ConvertTimeFromUtc(stop.Departure, TimeZoneInfo.Local):dd/MM/yy hh:mm}" });
-        Content = c;
     }
 }
