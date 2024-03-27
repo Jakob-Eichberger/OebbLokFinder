7702edd2c416f4fbe39464bf6372e5130816ff25
Jakob <jakob.eichberger@gmx.net>
2024-03-27 15:41:14 +0100

    chore: remove comments

--- OebbLokFinder/Model/Station.cs
+++ OebbLokFinder/Model/Station.cs
@@ -2,17 +2,13 @@
 {
     public class Station
     {
-        //public int Id { get; set; }
+        public string StationName { get; set; } = "";
 
-        public string StationName { get; set; }
-
-        public string StationNameAbbr { get; set; }
+        public string StationNameAbbr { get; set; } = "";
 
         public decimal Lat { get; set; }
 
         public decimal Lng { get; set; }
 
-        //public List<Stop> Stops { get; } = new();
     }
-
 }
