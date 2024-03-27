90f72f8cd87a3eea77c00ee5d9a99fee4035f364
Jakob <jakob.eichberger@gmx.net>
2024-03-27 17:35:28 +0100

    feat: Add functionality to RollingstockView

--- OebbLokFinder/View/Rollingstock/RollingstockView.xaml.cs
+++ OebbLokFinder/View/Rollingstock/RollingstockView.xaml.cs
@@ -1,15 +1,19 @@
 using OebbLokFinder.Model;
 using OebbLokFinder.Service;
+using System.ComponentModel;
 
 namespace OebbLokFinder.View.Rollingstock;
 
-public partial class RollingstockView : ContentView
+public partial class RollingstockView : ContentView, INotifyPropertyChanged
 {
     public RollingstockView(int rollingstockId)
     {
-        InitializeComponent();
         RollingstockId = rollingstockId;
+        RollingstockService = App.ServiceProvider.GetService<RollingstockService>();
+        InitializeComponent();
     }
+
+    public new event PropertyChangedEventHandler? PropertyChanged;
 
     public RollingstockService RollingstockService { get; set; } = default!;
 
@@ -22,10 +26,34 @@ public partial class RollingstockView : ContentView
         try
         {
             Rollingstock = await RollingstockService.GetRollingstockByIdAsync(RollingstockId) ?? throw new NullReferenceException();
+            BindingContext = Rollingstock;
+            PropertyChanged?.Invoke(this, new(nameof(Rollingstock)));
         }
         catch (Exception ex)
         {
             throw;
         }
+    }
+
+    private async void EntrClassNumber_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        await RollingstockService.UpdateRollingStockAsync(Rollingstock);
+    }
+
+    private async void EntrSerialNumber_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        await RollingstockService.UpdateRollingStockAsync(Rollingstock);
+
+    }
+
+    private async void EntrName_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        await RollingstockService.UpdateRollingStockAsync(Rollingstock);
+
+    }
+
+    private async void BtnDeletEntry_Pressed(object sender, EventArgs e)
+    {
+        await RollingstockService.RemoveRollingStockAsync(Rollingstock);
     }
 }
