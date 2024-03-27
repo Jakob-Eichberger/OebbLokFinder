116e9ccadfab58efa9984dc90dadc0f05349e2e6
Jakob <jakob.eichberger@gmx.net>
2024-03-27 11:14:38 +0100

    chore: sync namespace

--- OebbLokFinder/View/Converter/UtcToLocalDateTimeConverter.cs
+++ OebbLokFinder/View/Converter/UtcToLocalDateTimeConverter.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OebbLokFinder.Converter
+namespace OebbLokFinder.View.Converter
 {
     public class UtcToLocalDateTimeConverter : IValueConverter
     {
