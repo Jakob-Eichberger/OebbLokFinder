fb6ebda010a2b1e343c8b6c0f27f2eb8de2dcd66
Jakob <jakob.eichberger@gmx.net>
2024-03-28 08:58:05 +0100

    feat: Add Update Stops button to StopsPage

--- OebbLokFinder/View/Stops/StopsPage.xaml.cs
+++ OebbLokFinder/View/Stops/StopsPage.xaml.cs
@@ -10,18 +10,20 @@ public partial class StopsPage : ContentPage, INotifyPropertyChanged
     public StopsPage()
     {
         Db = App.ServiceProvider.GetRequiredService<Database>();
-        WebService = App.ServiceProvider.GetRequiredService<OebbWebService>();
+        SettingService = App.ServiceProvider.GetRequiredService<SettingService>();
+        OebbWebService = App.ServiceProvider.GetRequiredService<OebbWebService>();
         BindingContext = this;
         InitializeComponent();
     }
 
     public Database Db { get; set; }
+    public SettingService SettingService { get; set; }
 
     public string Station { get; set; }
 
     public List<string> Stations => Db.Stops.Where(e => (e.Arrival ?? e.Departure) >= DateTime.Now).Select(e => e.Station.StationName).Distinct().OrderBy(e => e).ToList();
 
-    public OebbWebService WebService { get; set; }
+    public OebbWebService OebbWebService { get; set; }
 
     private void Picker_SelectedIndexChanged(object sender, EventArgs e)
     {
@@ -30,6 +32,17 @@ public partial class StopsPage : ContentPage, INotifyPropertyChanged
         foreach (var stop in stops)
         {
             VSLStops.Add(new StopView(stop));
+        }
+    }
+
+    private async void BtnUpdateStops_Pressed(object sender, EventArgs e)
+    {
+        await Task.Delay(TimeSpan.FromSeconds(5));
+        var stock = Db.Rollingstocks.AsEnumerable().Where(e => SettingService.Setting.RefreshRollingStockCycle > 0 || !e.Stops.Any() || (e.Stops.Any() && (e.Stops.Max(e => e.Departure) - DateTime.Now).TotalHours < SettingService.Setting.RollingStockDataPreloadMin));
+        foreach (var item in stock)
+        {
+            await OebbWebService.UpdateStopsAsync(item);
+            await Task.Delay(TimeSpan.FromSeconds(3));
         }
     }
 }
