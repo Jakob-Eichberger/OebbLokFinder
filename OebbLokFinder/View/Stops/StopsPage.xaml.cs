4d97c1939ca150eb76bbfcb520c4778a3b154b5a
Jakob <jakob.eichberger@gmx.net>
2024-03-29 17:35:49 +0100

    feat; Add updateStops property, move code from BtnUpdateStops_Pressed to service

--- OebbLokFinder/View/Stops/StopsPage.xaml.cs
+++ OebbLokFinder/View/Stops/StopsPage.xaml.cs
@@ -7,23 +7,35 @@ namespace OebbLokFinder.View.Stops;
 
 public partial class StopsPage : ContentPage, INotifyPropertyChanged
 {
+    private bool updatingStops = true;
+
     public StopsPage()
     {
         Db = App.ServiceProvider.GetRequiredService<Database>();
-        SettingService = App.ServiceProvider.GetRequiredService<SettingService>();
-        OebbWebService = App.ServiceProvider.GetRequiredService<OebbWebService>();
+        StationService = App.ServiceProvider.GetRequiredService<StationService>();
         BindingContext = this;
         InitializeComponent();
     }
 
-    public Database Db { get; set; }
-    public SettingService SettingService { get; set; }
+    public new event PropertyChangedEventHandler? PropertyChanged;
 
-    public string Station { get; set; }
+    public Database Db { get; set; }
+
+    public StationService StationService { get; set; }
+
+    public string Station { get; set; } = string.Empty;
 
     public List<string> Stations => Db.Stops.Where(e => (e.Arrival ?? e.Departure) >= DateTime.Now).Select(e => e.Station.StationName).Distinct().OrderBy(e => e).ToList();
 
-    public OebbWebService OebbWebService { get; set; }
+
+    public bool UpdatingStops
+    {
+        get => updatingStops; set
+        {
+            updatingStops = value;
+            PropertyChanged?.Invoke(this, new(null));
+        }
+    }
 
     private void Picker_SelectedIndexChanged(object sender, EventArgs e)
     {
@@ -37,12 +49,18 @@ public partial class StopsPage : ContentPage, INotifyPropertyChanged
 
     private async void BtnUpdateStops_Pressed(object sender, EventArgs e)
     {
-        await Task.Delay(TimeSpan.FromSeconds(5));
-        var stock = Db.Rollingstocks.AsEnumerable().Where(e => SettingService.Setting.RefreshRollingStockCycle > 0 || !e.Stops.Any() || (e.Stops.Any() && (e.Stops.Max(e => e.Departure) - DateTime.Now).TotalHours < SettingService.Setting.RollingStockDataPreloadMin));
-        foreach (var item in stock)
+        try
         {
-            await OebbWebService.UpdateStopsAsync(item);
-            await Task.Delay(TimeSpan.FromSeconds(3));
+            UpdatingStops = false;
+            await StationService.UpdateStopsForRollingStockAsync();
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert($"Error while updating stops", ex.Message, "Ok");
+        }
+        finally
+        {
+            UpdatingStops = true;
         }
     }
 }
